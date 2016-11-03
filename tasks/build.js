b034ae6da7fceb8c5af2f67937acd3a1cf2a6619
Thunraz <thunraz@thunraz.com>
2016-11-03 09:32:41 +0100

    Adding gulp tasks

---
+++ tasks/build.js
@@ -0,0 +1,32 @@
+'use strict';
+
+let gulp        = require('gulp'),
+    rollup      = require('rollup').rollup;
+
+module.exports = () => {
+    gulp.task('copy-dependencies', () => {
+        return gulp
+            .src([
+                './src/ext/pixi.min.js',
+                './src/ext/pixi.min.js.map',
+
+                './src/electron/electron.js'
+            ])
+            .pipe(gulp.dest('./dist/'))
+    });
+
+    gulp.task('build', ['copy-dependencies'], () => {
+        return rollup({
+            entry: 'src/js/main.js',
+            sourceMap: true
+        }).then(function(bundle) {
+            return bundle.write({
+                dest:  'dist/main.js',
+                format: 'iife',
+                globals: {
+                    pixi:   'PIXI'
+                },
+            });
+        });
+    });
+};
